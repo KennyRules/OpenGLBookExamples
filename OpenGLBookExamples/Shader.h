8cbd1d50c83dc3f9d1a4b9802904ce491c385bf9
Drew Diamantoukos <drewd@code-phoenix.com>
2015-08-15 16:11:57 -0400

    Initial commit

---
+++ OpenGLBookExamples/Shader.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <memory>
+
+#include "sb7.h"
+#include "ShaderLoader.h"
+
+class Shader
+{
+private:
+    GLuint shader;
+    GLuint shaderType;
+    const char* shaderFileName;
+
+public:
+    Shader(GLuint aShaderType, GLchar* aFileName) :
+        shaderType(aShaderType), shaderFileName(aFileName)
+    {
+        GLchar* shaderSource[] = { "" };
+     
+        shader = glCreateShader(shaderType);
+        ShaderLoader::loadShader(shaderFileName, shaderSource);
+        glShaderSource(shader, 1, shaderSource, NULL);
+        glCompileShader(shader);
+
+        if (*shaderSource != nullptr)
+            delete[] * shaderSource;
+    }
+
+	GLuint getShader()
+    {
+        return shader;
+    }
+
+	~Shader()
+    {
+        glDeleteShader(shader);
+    }
+};
